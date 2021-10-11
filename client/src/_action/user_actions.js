0f45d48bc421aa819eabcdb0d970d946222f2deb
nyju <nyju2104@gmail.com>
2021-10-11 16:32:45 +0900

    회원가입 및 로그아웃

--- client/src/_action/user_actions.js
+++ client/src/_action/user_actions.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
 
 export function loginUser(dataToSubmit) {
-
-
     // 서버에서 받은 데이터를 request 변수에 저장
     const request = axios.post('/api/users/login', dataToSubmit)
         .then(response => response.data)
@@ -13,6 +12,18 @@ export function loginUser(dataToSubmit) {
 
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+export function registerUser(dataToSubmit) {
+    // 서버에서 받은 데이터를 request 변수에 저장
+    const request = axios.post('/api/users/register', dataToSubmit)
+        .then(response => response.data)
+
+
+    return {
+        type: REGISTER_USER,
         payload: request
     }
 }
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,12 +1,16 @@
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from '../_action/types';
 
-export default function(state = {}, action) {
+export default function (state = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
             // 스프레드오퍼레이터
             return { ...state, loginSuccess: action.payload }
+            break;
+        case REGISTER_USER:
+            return { ...state, register: action.payload }
             break;
 
         default:
